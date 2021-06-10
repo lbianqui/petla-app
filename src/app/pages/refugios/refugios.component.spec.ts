36f535d41ffa2a8dd718972af2da00819f5b345f
lbianquii <lucasgonzalo.bianqui@mercadolibre.com>
2021-06-09 22:15:20 -0300

    petla refugios

---
+++ src/app/pages/refugios/refugios.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RefugiosComponent } from './refugios.component';
+
+describe('RefugiosComponent', () => {
+  let component: RefugiosComponent;
+  let fixture: ComponentFixture<RefugiosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RefugiosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RefugiosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/refugios/refugios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-refugios',
+  templateUrl: './refugios.component.html',
+  styleUrls: ['./refugios.component.css']
+})
+export class RefugiosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
